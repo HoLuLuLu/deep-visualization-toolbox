86344e58b9e1be141a249f5959794b73b80e3b41
HoLuLuLu <schmolli.christian@googlemail.com>
2020-05-05 11:43:36 +0200

    Add new functions to generate_weights_histograms.py
    - option scale_by_layer: scale the weights histograms of every unit of a layer the same fro better comparability
    - option force: overwrite existing histogram files
    - normally skip histograms where a file already exist

--- generate_weights_histograms.py
+++ generate_weights_histograms.py
@@ -17,87 +17,97 @@ from optimize_image import change_model_to_load, clean_temp_file
 from settings_misc import load_network
 
 
-def calculate_bias_weights_histogram_for_specific_layer(net, layer_name, output_dir, fig, ax, do_print=True):
-    if do_print:
-        print "calculating weights histogram for layer %s" % (layer_name)
-
+def calculate_weights_histogram_for_specific_layer(net, layer_name, output_dir, fig, ax, force=False):
     # generate weights histogram for layer
+    hist_filename = os.path.join(output_dir, 'weight_histogram.png')
+
     weights = net.params[layer_name][0].data.flatten()
     hist, bin_edges = np.histogram(weights, bins=50)
-    weights_num = len(weights)
+    if not force and os.path.isfile(hist_filename):
+        print 'The weights histogram for this layer already exists. Skipping...'
+    else:
+        weights_num = len(weights)
 
-    width = 0.7 * (bin_edges[1] - bin_edges[0])
-    center = (bin_edges[:-1] + bin_edges[1:]) / 2
-    ax.bar(center, hist, align='center', width=width, color='g')
+        width = 0.7 * (bin_edges[1] - bin_edges[0])
+        center = (bin_edges[:-1] + bin_edges[1:]) / 2
+        ax.bar(center, hist, align='center', width=width, color='g')
 
-    fig.suptitle('weights for layer %s\n %s weights used' % (layer_name, weights_num))
-    ax.xaxis.label.set_text('weight value')
-    ax.yaxis.label.set_text('count')
+        fig.suptitle('weights for layer %s\n %s weights used' % (layer_name, weights_num))
+        ax.xaxis.label.set_text('weight value')
+        ax.yaxis.label.set_text('count')
 
-    # Save layer weights histogram to image file
-    mkdir_p(output_dir)
-    fig.savefig(os.path.join(output_dir, 'weight_histogram.png'))
+        # Save layer weights histogram to image file
+        mkdir_p(output_dir)
+        fig.savefig(hist_filename)
 
-    ax.cla()
+        ax.cla()
 
-    if do_print:
-        print "calculating bias histogram for layer %s" % (layer_name)
+    return hist, bin_edges
 
+
+def calculate_bias_histogram_for_specific_layer(net, layer_name, output_dir, fig, ax, force=False):
     # generate bias histogram for layer
-    bias = net.params[layer_name][1].data.flatten()
-    hist, bin_edges = np.histogram(bias, bins=50)
-    bias_num = len(bias)
+    bias_filename = os.path.join(output_dir, 'bias_histogram.png')
+    if not force and os.path.isfile(bias_filename):
+        print 'The bias histogram for this layer already exists. Skipping...'
+    else:
+        bias = net.params[layer_name][1].data.flatten()
+        hist, bin_edges = np.histogram(bias, bins=50)
+        bias_num = len(bias)
 
-    width = 0.7 * (bin_edges[1] - bin_edges[0])
-    center = (bin_edges[:-1] + bin_edges[1:]) / 2
-    ax.bar(center, hist, align='center', width=width, color='g')
+        width = 0.7 * (bin_edges[1] - bin_edges[0])
+        center = (bin_edges[:-1] + bin_edges[1:]) / 2
+        ax.bar(center, hist, align='center', width=width, color='g')
 
-    fig.suptitle('bias for layer %s\n %s biases used' % (layer_name, bias_num))
-    ax.xaxis.label.set_text('bias value')
-    ax.yaxis.label.set_text('count')
+        fig.suptitle('bias for layer %s\n %s biases used' % (layer_name, bias_num))
+        ax.xaxis.label.set_text('bias value')
+        ax.yaxis.label.set_text('count')
 
-    # Save layer bias histogram to image file
-    fig.savefig(os.path.join(output_dir, 'bias_histogram.png'))
+        # Save layer bias histogram to image file
+        fig.savefig(bias_filename)
 
-    ax.cla()
+        ax.cla()
+
+        return hist, bin_edges
 
 
 def calculate_weights_histogram_for_specific_unit(net, layer_name, channel_idx, output_dir, fig, ax,
-                                                  hist_range=(None, None), do_print=True):
-    n_channels = net.params[layer_name][0].shape[0]
+                                                  hist_range=(None, None), force=False):
+    hist_filename = os.path.join(output_dir, 'weight_histogram.png')
+    if not force and os.path.isfile(hist_filename):
+        print 'The weights histogram for unit %d already exists. Skipping...' % (channel_idx)
+    else:
+        # get vector of weights
+        weights = net.params[layer_name][0].data[channel_idx].flatten()
+        bias = net.params[layer_name][1].data[channel_idx]
+        weights_num = len(weights)
 
-    if do_print and channel_idx % 10 == 0:
-        print "calculating weights histogram for layer %s channel %d out of %d" % (
-            layer_name, channel_idx, n_channels)
+        # create histogram
+        if hist_range == (None, None):
+            hist_range = (weights.min(), weights.max())
+        hist, bin_edges = np.histogram(weights, bins=50, range=hist_range)
 
-    # get vector of weights
-    weights = net.params[layer_name][0].data[channel_idx].flatten()
-    bias = net.params[layer_name][1].data[channel_idx]
-    weights_num = len(weights)
+        # generate histogram image file
+        width = 0.7 * (bin_edges[1] - bin_edges[0])
+        center = (bin_edges[:-1] + bin_edges[1:]) / 2
 
-    # create histogram
-    if hist_range is (None, None):
-        hist_range = (weights.min(), weights.max())
-    hist, bin_edges = np.histogram(weights, bins=50, range=hist_range)
+        ax.bar(center, hist, align='center', width=width, color='g')
 
-    # generate histogram image file
-    width = 0.7 * (bin_edges[1] - bin_edges[0])
-    center = (bin_edges[:-1] + bin_edges[1:]) / 2
+        fig.suptitle('weights for unit %d, bias is %f\n %s weights used' % (channel_idx, bias, weights_num))
+        ax.xaxis.label.set_text('weight value')
+        ax.yaxis.label.set_text('count')
 
-    ax.bar(center, hist, align='center', width=width, color='g')
+        # save weight histogram as image file
+        mkdir_p(output_dir)
+        fig.savefig(hist_filename)
 
-    fig.suptitle('weights for unit %d, bias is %f\n %s weights used' % (channel_idx, bias, weights_num))
-    ax.xaxis.label.set_text('weight value')
-    ax.yaxis.label.set_text('count')
+        ax.cla()
 
-    # save weight histogram as image file
-    mkdir_p(output_dir)
-    fig.savefig(os.path.join(output_dir, 'weight_histogram.png'))
-
-    ax.cla()
+        return hist, bin_edges
 
 
-def calculate_weight_bias_histograms_for_net(net, settings, output_dir=None):
+def calculate_weight_bias_histograms_for_net(net, settings, output_dir=None, scale_by_layer=False, do_print=True,
+                                             force=False):
     if not output_dir:
         output_dir = settings.caffevis_outputs_dir
 
@@ -107,14 +117,34 @@ def calculate_weight_bias_histograms_for_net(net, settings, output_dir=None):
     ax = fig.add_subplot(111)
 
     for layer_name in layers:
+        if do_print:
+            print "calculating weights and bias histogram for layer %s" % (layer_name)
+
         layer_output_dir = os.path.join(output_dir, layer_name)
         mkdir_p(layer_output_dir)
-        calculate_bias_weights_histogram_for_specific_layer(net, layer_name, layer_output_dir, fig, ax)
+        try:
+            layer_hist, layer_bin_edges = calculate_weights_histogram_for_specific_layer(net, layer_name,
+                                                                                         layer_output_dir, fig, ax,
+                                                                                         force)
+            calculate_bias_histogram_for_specific_layer(net, layer_name, layer_output_dir, fig, ax, force)
 
-        for channel_idx in xrange(0, net.params[layer_name][0].shape[0]):
-            unit_output_dir = os.path.join(layer_output_dir, 'unit_%04d' % channel_idx)
-            mkdir_p(unit_output_dir)
-            calculate_weights_histogram_for_specific_unit(net, layer_name, channel_idx, unit_output_dir, fig, ax)
+            for channel_idx in xrange(0, net.params[layer_name][0].shape[0]):
+                n_channels = net.params[layer_name][0].shape[0]
+
+                if do_print and channel_idx % 10 == 0:
+                    print "calculating weights histogram for layer %s channel %d out of %d" % (
+                        layer_name, channel_idx, n_channels)
+
+                unit_output_dir = os.path.join(layer_output_dir, 'unit_%04d' % channel_idx)
+                mkdir_p(unit_output_dir)
+                if scale_by_layer and not layer_bin_edges is None:
+                    layer_hist_range = (layer_bin_edges.min(), layer_bin_edges.max())
+                else:
+                    layer_hist_range = (None, None)
+                calculate_weights_histogram_for_specific_unit(net, layer_name, channel_idx, unit_output_dir, fig, ax,
+                                                              layer_hist_range, force)
+        except KeyError as excep:
+            print 'Unable to generate weights/bias histograms for this layer: ' + str(excep)
 
 
 def calculate_weights_histograms():
@@ -127,25 +157,19 @@ def calculate_weights_histograms():
 
         parser = argparse.ArgumentParser(
             description='Generates weights histograms for layers and its units.')
-        parser.add_argument('--gpu', action='store_true', default=settings.caffevis_mode_gpu, help='use gpu')
-        parser.add_argument('--net_prototxt', type=str, default=settings.caffevis_deploy_prototxt,
-                            help='network prototxt to load')
-        parser.add_argument('--net_weights', type=str, default=settings.caffevis_network_weights,
-                            help='network weights to load')
+        parser.add_argument('--force', action='store_true', default=False,
+                            help='Activate force mode. Overwrites existing files')
+        parser.add_argument('--scale_by_layer', action='store_true', default=False,
+                            help='Every unit of a layer has the same scale for the histogram.')
         parser.add_argument('--outdir', type=str, default=settings.caffevis_outputs_dir,
                             help='Which output directory to use. Files are output into outdir/layer/unit_%%04d/{weights_histogram}.png')
         parser.add_argument('--model', type=str, default=None,
                             help='Name of the model you want to change to. This overwrites the settings made in files.')
         args = parser.parse_args()
 
-        settings.caffevis_deploy_prototxt = args.net_prototxt
-        settings.caffevis_network_weights = args.net_weights
-
         net, data_mean = load_network(settings)
-        calculate_weight_bias_histograms_for_net(net, settings, args.outdir)
+        calculate_weight_bias_histograms_for_net(net, settings, args.outdir, args.scale_by_layer, force=args.force)
 
-    except Exception as exep:
-        print str(exep)
     finally:
         clean_temp_file()
 
